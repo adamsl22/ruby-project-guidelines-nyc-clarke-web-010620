742b72017be118348d272ad219f1392a79b4826d
arichards4814 <arichards4814@gmail.com>
2020-01-23 16:02:26 -0500

    fixed end game error maybe

--- tinyGameEngine/gameevent.rb
+++ tinyGameEngine/gameevent.rb
@@ -78,7 +78,7 @@ def self.check_for_loss
     if Dragon.all.count == 0 && Dragon.eggs == 0
         UI.announce("You have no dragons or dragon eggs! Game over!", "red")
         exit
-    elsif Village.all == nil
+    elsif Village.all == nil || Village.all == []
         UI.announce("You have exhausted the food supply! Game over!", "red")
         exit
     end
